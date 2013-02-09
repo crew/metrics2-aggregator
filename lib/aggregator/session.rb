7d37a9240a3948abc95a74cb633ee3ac1ec54284
Ali Ukani <ali.ukani@gmail.com>
2013-02-09 15:00:14 -0500

    Don't store the user or IP of someone that's logged in. That's invasive, bro.

--- lib/aggregator/session.rb
+++ lib/aggregator/session.rb
@@ -1,23 +1,23 @@
 # Represents an entry in the raw log database
 module Aggregator
   class Session
-    attr_accessor :user, :machine, :os, :source, :ip, :started,
-      :ended, :set
+    attr_accessor :machine, :os, :source, :started, :ended, :set
 
-    def initialize(user, machine, os, source, ip, started, ended, set)
-      @user = user
-      @machine = machine
-      @os = os
-      @source = source
-      @ip = ip
-      @started = started
-      @ended = ended
-      @set = set
+    def initialize(hostname, os, source, started, ended, set)
+      @hostname = hostname  # machine hostname
+      @os = os              # linux or windows
+      @source = source      # tty or pts
+      @started = started    # when the session was started
+      @ended = ended        # when the session ended
+      @set = set            # the timestamp for when this data was grabbed
+      instance_variables.map {|var| var.lowercase! if var.is_a? String }
     end
 
     def to_hash
       hash = {}
-      instance_variables.each {|var| hash[var.to_s.delete("@").intern] = instance_variable_get(var) }
+      instance_variables.each do |var|
+        hash[var.to_s.delete("@").intern] = instance_variable_get(var)
+      end
       hash
     end
 
